362232d7f61c087c3ddcf346786dc9c054137d23
sahithanusuri <sahithanusuri@gmail.com>
2022-11-08 09:08:27 -0600

    added index.js

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function mondayFunction() {
+   var element = document.getElementById("idmonday");
+   element.classList.toggle("togglemonday");
+}
+
+function tuesdayFunction() {
+   var element = document.getElementById("idtuesday");
+   element.classList.toggle("toggletuesday");
+}
+
+function wednesdayFunction() {
+   var element = document.getElementById("idwednesday");
+   element.classList.toggle("togglewednesday");
+}
+
+function thursdayFunction() {
+   var element = document.getElementById("idthursday");
+   element.classList.toggle("togglethursday");
+}
+
+function fridayFunction() {
+   var element = document.getElementById("idfriday");
+   element.classList.toggle("togglefriday");
+}
+
+function saturdayFunction() {
+   var element = document.getElementById("idsaturday");
+   element.classList.toggle("togglesaturday");
+}
+
+function sundayFunction() {
+   var element = document.getElementById("idsunday");
+   element.classList.toggle("togglesunday");
+}
