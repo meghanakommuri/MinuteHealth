6b7ca16e7fbcc2864aa2ba82d1dc546a28146d56
meghanakommuri <meghanakommuri@gmail.com>
2022-11-11 09:35:36 -0600

    Added carousel on get started page

--- js/register.js
+++ js/register.js
@@ -38,7 +38,7 @@ function validateEmail(email) {
     }
     else {
       if(!validateName(name)){
-        result.text("Invalid name. Name can contain only alphabets.");
+        result.text("Invalid username. Name can contain only alphabets.");
         result.css("color", "red");
         return false;
       }
