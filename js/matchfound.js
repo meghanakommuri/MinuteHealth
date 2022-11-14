55a0db6d9b88a52ffb9e9618794a68dcf72b5c17
sahithanusuri <sahithanusuri@gmail.com>
2022-11-14 10:45:47 -0600

    Matching based on age

---
+++ js/matchfound.js
@@ -0,0 +1,25 @@
+// // document.getElementById("write").innerHTML = window.location.search;
+console.log("matchfound page is working");
+// console.log(window.location.search);
+var queryString = window.location.search.replace(/^\?/, '');
+// console.log(queryString);
+const myArray = queryString.split("&");
+// console.log(myArray);
+var name = myArray[0].slice(5);
+var age = myArray[1].slice(4);
+var contact = myArray[2].slice(8);
+// console.log(name);
+// console.log(age);
+// console.log(contact);
+
+
+
+var keys=Object.keys(localStorage);
+for(var key of keys){
+  if(localStorage.getItem(key) == age ){
+    console.log("Age Matched")
+  };
+}
+
+
+localStorage.setItem(name,age);
--- js/medpalform.js
+++ js/medpalform.js
@@ -3,13 +3,11 @@ var findmatch=document.getElementById("findmatch")
 var age=document.getElementById("age")
 var fname=document.getElementById("fname")
 
-findmatch.onclick=function(){
-  console.log("before");
-    localStorage.setItem(fname.value,age.value);
-    var keys = Object.keys(localStorage);
-    console.log("after");
-    console.log(keys);
-}
+// findmatch.onclick=function(){
+//     localStorage.setItem(fname.value,age.value);
+//     var keys = Object.keys(localStorage);
+//     console.log(keys);
+// }
 
 // findmatch.onclick=function(){
 //   console.log("inside findmatch function");
@@ -21,9 +19,9 @@ findmatch.onclick=function(){
 //       if(localStorage.getItem(key) == age ){
 //         console.log("Age Matched")
 //       };
-//       // if(key == "key1"){
-//       //   console.log("Key : "+key+" : Value : "+localStorage.getItem(key));
-//       // }
+//       if(key == "key1"){
+//         console.log("Key : "+key+" : Value : "+localStorage.getItem(key));
+//       }
 //
 //     }
 // }
