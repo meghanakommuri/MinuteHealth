aa54c57368e517186b706a3a6057a8ec4cbe35bc
sahithanusuri <sahithanusuri@gmail.com>
2022-11-14 15:32:35 -0600

    updated

--- js/matchfound.js
+++ js/matchfound.js
@@ -1,39 +1,24 @@
-// // document.getElementById("write").innerHTML = window.location.search;
-console.log("matchfound page is working");
-// console.log(window.location.search);
 var queryString = window.location.search.replace(/^\?/, '');
 const myArray = queryString.split("&");
-// const newArray = [];
-// console.log(myArray);
 var name = myArray[0].slice(5);
 var age = myArray[1].slice(4);
 var medicinename = myArray[2].slice(13);
 
-// console.log(name);
-// console.log(age);
-// console.log(medicinename);
 
 var matchname=document.getElementById("matchname");
 var matchmedicine=document.getElementById("matchmedicine");
 var matchage=document.getElementById("matchage");
-
-// var age = document.getElementById("age");
-
-
-
 var keys=Object.keys(localStorage);
+
+
 for(var key of keys){
   if(key == name){continue;}
-  console.log(localStorage.getItem(key));
-
-
-  if(localStorage.getItem(key).slice(3) == medicinename ){
+  var substringarray = localStorage.getItem(key).split(",");
+  if(substringarray[1] == medicinename ){
     matchname.innerHTML = "Name: "+key;
-    matchmedicine.innerHTML="Meicine Name: "+medicinename;
-    matchage.innerHTML="Age: "+localStorage.getItem(key).slice(0,2);
-    console.log("Medicine Matched");
+    matchmedicine.innerHTML="Meicine: "+medicinename;
+    matchage.innerHTML="Age: "+substringarray[0];
   };
 }
-
 
 localStorage.setItem(name,[age,medicinename]);
